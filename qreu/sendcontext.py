975168a19d316ed4d3ab49d04e658ff6bcaabf72
Jaume Florez <jflorez@gisce.net>
2018-01-16 17:19:09 +0100

    Add SMTPSender class on sendcontext.py

--- qreu/sendcontext.py
+++ qreu/sendcontext.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from qreu import local
+from smtplib import SMTP
 
 _SENDCONTEXT = local.LocalStack()
 
@@ -13,7 +14,7 @@ class Sender(object):
         _SENDCONTEXT.push(self)
         return _SENDCONTEXT.top
 
-    def __exit__(self, a, b, c):
+    def __exit__(self, etype, evalue, etraceback):
         _SENDCONTEXT.pop()
 
     def send(self, mail):
@@ -39,3 +40,38 @@ class FileSender(Sender):
         """
         with open(self._filename, 'w') as writer:
             writer.write(mail.mime_string)
+
+class SMTPSender(Sender):
+    def __init__(
+            self, host='localhost', port=0, user='', passwd='',
+            ssl_keyfile='', ssl_certfile=''
+    ):
+        super(SMTPSender, self).__init__(
+            _host=host,
+            _port=port,
+            _user=user,
+            _passwd=passwd,
+            _ssl_keyfile=ssl_keyfile,
+            _ssl_certfile=ssl_certfile
+        )
+
+    def __enter__(self):
+        self._connection = SMTP(host=self._host, port=self._port)
+        if self._ssl_keyfile and self._ssl_certfile:
+            self._connection.starttls(
+                keyfile=self._ssl_keyfile, certfile=self._ssl_certfile)
+        if self._user and self._passwd:
+            self._connection.login(user=self._user, password=self._passwd)
+        return super(SMTPSender, self).__enter__()
+
+    def __exit__(self, etype, evalue, etraceback):
+        super(SMTPSender, self).__exit__(etype, evalue, etraceback)
+        self._connection.close()
+
+    def send(self, mail):
+        """
+        Send the qreu.Email object through smtp.sendmail
+        :param mail:    qreu.Email object to send
+        :type mail:     Email
+        """
+        self._connection.sendmail(mail.from_, mail.recipients, mail.mime_string)
