e6c3ba763130c87de68e87b1da6d6157d5b97d93
Jaume Florez <jflorez@gisce.net>
2018-01-16 15:39:48 +0100

    Add default sender test

--- spec/sendcontext_spec.py
+++ spec/sendcontext_spec.py
@@ -7,19 +7,10 @@ from qreu import Email
 from mamba import *
 from expects import *
 
-with description('Simple SenderManager'):
-    with context('simple configuration'):
-        with before.all:
-            def send_true(mail):
-                return True
-            self.manager = sendcontext.SenderContext()
-            self.send_true = send_true
-            self.test_mail = Email()
-        with it('must create a new simple configuration'):
-            expect(self.manager.has_configs()).to(be_false)
-            self.manager.add_config(send_method=self.send_true)
-            expect(self.manager.has_configs()).to(be_true)
-
-        with it('must "send" correctly any email from config'):
-            for config in self.manager.configs:
-                expect(config.send(self.test_mail)).to(be_true)
+with description('Sendcontext'):
+    with before.all:
+        self.test_mail = Email()
+    with it('must return the mail as a string with default Sender'):
+        with sendcontext.Sender() as sender:
+            expect(sender.send(self.test_mail)).to(
+                equal(self.test_mail.mime_string))
