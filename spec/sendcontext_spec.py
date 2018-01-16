97fb0af70c8f615b6c7ece6005f6b2b34acac123
Jaume Florez <jflorez@gisce.net>
2018-01-16 12:56:48 +0100

    Split tests with the same context values

--- spec/sendcontext_spec.py
+++ spec/sendcontext_spec.py
@@ -2,18 +2,24 @@
 from __future__ import absolute_import, unicode_literals
 
 from qreu import sendcontext
+from qreu import Email
 
 from mamba import *
 from expects import *
 
 with description('Simple SenderManager'):
-    with it(
-        'must create a new configuration, add it to SenderManager'
-        ' and be able to send'
-    ):
-        def send_true():
-            return True
-        manager = sendcontext.SenderContext()
-        manager.add_config(send_method=send_true)
-        expect(manager.has_configs()).to(be_true)
-        expect([c.send() for c in manager.configs]).to(equal([True]))
+    with context('simple configuration'):
+        with before.all:
+            def send_true(mail):
+                return True
+            self.manager = sendcontext.SenderContext()
+            self.send_true = send_true
+            self.test_mail = Email()
+        with it('must create a new simple configuration'):
+            expect(self.manager.has_configs()).to(be_false)
+            self.manager.add_config(send_method=self.send_true)
+            expect(self.manager.has_configs()).to(be_true)
+
+        with it('must "send" correctly any email from config'):
+            for config in self.manager.configs:
+                expect(config.send(self.test_mail)).to(be_true)
