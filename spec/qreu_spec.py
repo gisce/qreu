7e29f7579ea956163a9282d40e0e448c170caa9e
Jaume Florez <jflorez@gisce.net>
2018-01-18 17:13:52 +0100

    Update the tests as the res of decode_header
    - decode_header removes the space between display name and address

--- spec/qreu_spec.py
+++ spec/qreu_spec.py
@@ -312,12 +312,13 @@ with description("Creating an Email"):
 
         with it('must add addresses correctly as "name" <address>'):
             address = 'spécial <special@example.com>'
+            parsed = 'spécial<special@example.com>'
             e = Email(to=address)
-            expect(e.to).to(equal([address]))
+            expect(e.to).to(equal([parsed]))
             e = Email(cc=address)
-            expect(e.cc).to(equal([address]))
+            expect(e.cc).to(equal([parsed]))
             e = Email(bcc=[address])
-            expect(e.bcc).to(equal([address]))
+            expect(e.bcc).to(equal([parsed]))
 
         with it('must parse html2text if no text provided'):
             vals = self.vals.copy()
