4fe67727e71f071281d774959af2553cfaa338af
Eduard Berloso <eberloso@gisce.net>
2019-01-18 10:50:35 +0100

    Bump to v0.7.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qreu',
-    version='0.7.4',
+    version='0.7.5',
     packages=find_packages(),
     url='https://github.com/gisce/qreu',
     install_requires=[
