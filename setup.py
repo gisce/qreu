2f026fe534d61ee37110f09d2e4fc6b5c4ef236b
PSala <polsalalidon@gmail.com>
2023-10-04 16:42:33 +0200

    Bump to v0.11.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qreu',
-    version='0.10.1',
+    version='0.11.0',
     packages=find_packages(),
     url='https://github.com/gisce/qreu',
     install_requires=[
