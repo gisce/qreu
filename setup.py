3ddbb9c67edaa6e4fe0356f218ced9a19468e89f
Release <github-actions@github.com>
2025-07-30 14:15:03 +0200

    Bump to v0.14.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qreu',
-    version='0.13.3',
+    version='0.14.0',
     packages=find_packages(),
     url='https://github.com/gisce/qreu',
     install_requires=[
